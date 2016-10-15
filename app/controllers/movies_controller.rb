ff31109e5c03b948c82c1d50c19821803d83c3a1
richadeo <richadeo@tamu.edu>
2016-10-15 17:53:19 -0500

    sort_movie_title

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -43,7 +43,8 @@ class MoviesController < ApplicationController
   end
   
   def sort
-     @movies.sort_by!{ |m| m[:movie][:title] }
+     @movies.sort_by!{ |m| m.movie.title }
+     redirect_to movies_path
   end
 
 end
